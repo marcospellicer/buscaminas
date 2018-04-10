2fcc6eae2407bf76d551702bd4cee911d2c260a7
alumno <alumno@aula01-09>
2018-04-10 19:25:58 +0200

    principio

---
+++ Buscaminas/src/buscaminas/Casilla.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buscaminas;
+
+/**
+ *
+ * @author alumno
+ */
+public class Casilla {
+    private boolean mina;
+    private boolean blanco;
+    private boolean bandera;
+    private boolean visible;
+    private int  numero;
+
+    public Casilla() {
+        this.mina = false;
+        this.blanco = false;
+        this.bandera = false;
+        this.visible = false;
+        this.numero = 0;
+    }
+
+
+    /**
+     * @return the mina
+     */
+    public boolean isMina() {
+        return mina;
+    }
+
+    /**
+     * @param mina the mina to set
+     */
+    public void setMina(boolean mina) {
+        this.mina = mina;
+    }
+
+    /**
+     * @return the blanco
+     */
+    public boolean isBlanco() {
+        return blanco;
+    }
+
+    /**
+     * @param blanco the blanco to set
+     */
+    public void setBlanco(boolean blanco) {
+        this.blanco = blanco;
+    }
+
+    /**
+     * @return the bandera
+     */
+    public boolean isBandera() {
+        return bandera;
+    }
+
+    /**
+     * @param bandera the bandera to set
+     */
+    public void setBandera(boolean bandera) {
+        this.bandera = bandera;
+    }
+
+    /**
+     * @return the visible
+     */
+    public boolean isVisible() {
+        return visible;
+    }
+
+    /**
+     * @param visible the visible to set
+     */
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
+    /**
+     * @return the numero
+     */
+    public int getNumero() {
+        return numero;
+    }
+
+    /**
+     * @param numero the numero to set
+     */
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    
+    
+}
---
+++ Buscaminas/src/buscaminas/Tablero.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package buscaminas;
+
+/**
+ *
+ * @author alumno
+ */
+public class Tablero {
+    private int numFilas;
+    private int numColumnas;
+    private int numMinas;
+    private Casilla[][] tabla;
+    
+    
+}
